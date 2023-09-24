e8f2366b1f7475396df93af1396d7367f917c04a
PRATIK LOKHANDE <pratiklokhande@PRATIKs-MacBook-Air.local>
2023-09-25 00:56:44 +0530

    StaticHtmlCSSServe

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const express = require('express');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin',adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req,res,next) =>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
+
+
+
+const server = http.createServer(app);
+
+server.listen(8080); 
